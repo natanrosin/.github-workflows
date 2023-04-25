c30e5b8a03f377e2699540a5dfd0a6239a52a4e4
natanrosin <86538206+natanrosin@users.noreply.github.com>
2023-04-24 23:25:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask
+print "hello world"
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
